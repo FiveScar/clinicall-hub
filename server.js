90160327ab6af6ea706c6129c6c522f691310af2
FiveScar <shieldseg01@gmail.com>
2026-02-03 14:03:41 -0300

    fix: bind server to 0.0.0.0 and dynamic port

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
-// server.js
 import app from "./src/app.js";
 
 const PORT = process.env.PORT || 3333;
-app.listen(PORT, "0.0.0.0", () => console.log(`API running on ${PORT}`));
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Clinicall Hub running on port ${PORT}`);
+});
