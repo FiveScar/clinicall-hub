eca89063d8652dd1837f17082085c47908ca6785
FiveScar <shieldseg01@gmail.com>
2026-02-03 16:49:56 -0300

    fix: companies routes use partners/company

--- src/routes/companies.routes.js
+++ src/routes/companies.routes.js
@@ -4,13 +4,29 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 
 const router = Router();
 
-// POST /companies/search
-router.post(
-  "/search",
+/**
+ * GET /companies
+ * -> Clinicall: GET /partners/company
+ */
+router.get(
+  "/",
+  asyncHandler(async (_req, res) => {
+    const data = await clinicall.request("/partners/company", { method: "GET" });
+    res.json(data);
+  })
+);
+
+/**
+ * GET /companies/:companyId
+ * -> Clinicall: GET /partners/company/:companyId
+ */
+router.get(
+  "/:companyId",
   asyncHandler(async (req, res) => {
-    const data = await clinicall.request("/partners/company/search", {
-      method: "POST",
-      body: req.body,
+    const { companyId } = req.params;
+
+    const data = await clinicall.request(`/partners/company/${companyId}`, {
+      method: "GET",
     });
 
     res.json(data);
